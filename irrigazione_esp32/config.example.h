b52225f47a37c8cf2f0d0933b26e52e4e1987e81
Sergio <sergio.cordedda@abinsula.com>
2026-04-07 10:30:29 +0200

    Add example files

---
+++ irrigazione_esp32/config.example.h
@@ -0,0 +1,12 @@
+// ── WiFi ──────────────────────────────────────────────────────────────────────
+#define WIFI_SSID      "SSID"
+#define WIFI_PASSWORD  "password"
+
+// ── MQTT ──────────────────────────────────────────────────────────────────────
+#define MQTT_BROKER    "IP"   // IP del Raspberry Pi nella tua LAN
+#define MQTT_PORT      1883
+
+// ── OTA — GitHub ──────────────────────────────────────────────────────────────
+// Inserisci il tuo username GitHub e il nome del repository
+#define GITHUB_USER    "user"
+#define GITHUB_REPO    "name_repo"
